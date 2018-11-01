d62c9cb0fcbc4048c91d8e4ff205b6812d1c3c3c
DavidARoy <roy.david6th@gmail.com>
2018-11-01 22:13:20 +0000

    I hope this works

--- owl/routes/index.js
+++ owl/routes/index.js
@@ -6,7 +6,7 @@ var request = require('request');
 router.get('/', function(req, res, next) {
     res.sendFile('weather.html', { root: 'public' });
 });
-
+/*
 router.get('/getcity', function(req, res, next) {
     console.log("In getcity route");
     var fs = require('fs');
@@ -34,6 +34,31 @@ router.get('/getcity', function(req, res, next) {
         res.status(200).json(jsonresult);
     });
 });
+*/
+router.get('/randomWord', function(req, res, next) {
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    console.log("In random word route");
+    var fs = require('fs');
+    var words;
+
+    fs.readFile(__dirname + "/words.dat.txt", function(err, data) {
+        if (err) throw err;
+        words = data.toString().split("\n");
+        console.log(words);
+        var index = getRandomInt(words.length);
+        console.log(index);
+        var word = words[index];
+        console.log(word);
+        var url = "https://owlbot.info/api/v1/dictionary/";
+        url += word;
+        url += "?format=json";
+        console.log(url);
+        request(url).pipe(res);
+    })
+})
 
 router.get('/dictionary', function(req, res, next) {
     var url = "https://owlbot.info/api/v1/dictionary/";
